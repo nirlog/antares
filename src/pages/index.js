64d7f7e4a3f86010426d170592facc92473d9dee
Aleksandr Lebedev <nirlog@yandex.ru>
2022-08-02 12:56:24 +0300

    Desktop верстка

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,40 @@ import 'owl.carousel/dist/assets/owl.carousel.css';
 import './index.css';
 import 'owl.carousel';
 
-// start Top Menu
-const buttonOpenTopMenu = document.querySelector('.button-menu');
+
+// start Fixed Desktop Top Menu
+document.addEventListener('scroll', function(){
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const headerContent = document.querySelector('.header');
+    if(headerContent){
+        if(document.documentElement.clientWidth > 1000 && scrollTop > 120){
+            headerContent.classList.add('header_sticky');
+        } else if(document.documentElement.clientWidth > 1000 && scrollTop <= 100){
+            headerContent.classList.remove('header_sticky');
+        } else if (document.documentElement.clientWidth <= 1000){
+            headerContent.classList.remove('header_sticky');
+        }
+    }
+});
+// end Fixed Desktop Top Menu
+
+
+// start Mobile Top Menu
+const buttonOpenTopMenu = document.querySelector('.mobile-button-menu');
 if(buttonOpenTopMenu){
-    const topMenu = document.querySelector('.top-menu');
+    const topMenu = document.querySelector('.mobile-top-menu');
     if(topMenu){
-        buttonOpenTopMenu.addEventListener('click', function(){
+        buttonOpenTopMenu.addEventListener('click', function(e){
+            e.preventDefault();
             topMenu.classList.add('active');
             if(topMenu.classList.contains('active')){
                 topMenu.style.height = `${topMenu.scrollHeight}px`;
             }
         });
-        const buttonCloseTopMenu = document.querySelector('.top-menu__close');
+        const buttonCloseTopMenu = document.querySelector('.mobile-top-menu__close');
         if(buttonCloseTopMenu){
-            buttonCloseTopMenu.addEventListener('click', function(){
+            buttonCloseTopMenu.addEventListener('click', function(e){
+                e.preventDefault();
                 topMenu.classList.remove('active');
                 window.getComputedStyle(topMenu, null).getPropertyValue("height");
                 topMenu.style.height = "0";
@@ -24,19 +44,22 @@ if(buttonOpenTopMenu){
         }
     }
 }
-// end Top Menu
+// end Mobile Top Menu
 
 // start Mobile Search
-const buttonMobileSearch = document.querySelector('.list-buttons__search');
+const buttonMobileSearch = document.querySelector('.mobile-list-buttons__search');
 if(buttonMobileSearch){
     const mobileSearch = document.querySelector('.mobile-search');
     if(mobileSearch){
-        buttonMobileSearch.addEventListener('click', function(){
+        buttonMobileSearch.addEventListener('click', function(e){
+            e.preventDefault();
             buttonMobileSearch.classList.toggle('active');
             mobileSearch.classList.toggle('active');
+            
         });
         const buttonCloseMobileSearch= document.querySelector('.mobile-search__button-close');
-        buttonCloseMobileSearch.addEventListener('click', function(){
+        buttonCloseMobileSearch.addEventListener('click', function(e){
+            e.preventDefault();
             buttonMobileSearch.classList.remove('active');
             mobileSearch.classList.remove('active');
         });
